e7548d5efec5b0cd96e9fe0d6035059cd483f55d
daiiacinta7 <32591374+daiiacinta7@users.noreply.github.com>
2017-10-07 16:54:43 +0700

    Update call.js

--- call.js
+++ call.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/10/printed-circuit-board-layout.html"}
-if(x=="2"){xxx="2017/10/pcb-design-steps-layout-using-eagle.html"}
+if(x=="2"){xxx="/2017/10/pcb-design-steps-layout-using-eagle.html"}
 if(x=="3"){xxx="/2017/10/the-list-of-pcb-board-price.html"}
 if(x=="4"){xxx="/2017/10/pcb-electronics-contract-manufacturers.html"}
 if(x=="5"){xxx="/2017/10/pcb-design-tutorial-using-eagle-cad.html"}a_to_ve[i].href="http://www.pcbboarddesign.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
